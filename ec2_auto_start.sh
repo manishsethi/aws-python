844ed271b5342426bf4e11296cac6296434d7a21
manishsethi <manish@sethis.in>
2015-06-19 15:13:44 +0530

    bash script to trigger stop/start

--- ec2_auto_start.sh
+++ ec2_auto_start.sh
@@ -1,6 +1,6 @@
 
 #!/bin/bash
-#instancelist.txt contains the list instance tag you want to stop
+#instancelist.txt contains list instance tag you want to start
 FILE="instancelist.txt"
 while read line; do
     python ec2-state-change.py start $line
